5d5a9ff8cba5c1ea3761958cfd3bbcc6d6be5b93
Paul Bouquet <paul.bouquet@gmail.com>
2017-07-09 19:50:38 +0200

    init2

---
+++ app/src/main/java/com/t/projetx/Utils/Localization.java
@@ -0,0 +1,8 @@
+package com.t.projetx.Utils.Database;
+
+/**
+ * Created by Paul on 09/07/2017.
+ */
+
+public class Localization {
+}
--- app/src/test/java/com/t/projetx/ExampleUnitTest.java
+++ app/src/test/java/com/t/projetx/ExampleUnitTest.java
@@ -11,7 +11,6 @@ import static org.junit.Assert.*;
  */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
+    public void addition_isCorrect(){
     }
 }
