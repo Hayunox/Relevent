a63d4bd69852c8b4294c72d01d15bdf69d0b2220
Paul Bouquet <paul.bouquet@gmail.com>
2017-07-09 23:48:55 +0200

    #3 SQL file for database creation

--- app/src/test/java/com/teamX/projetx/ExampleUnitTest.java
+++ app/src/test/java/com/teamX/projetx/ExampleUnitTest.java
@@ -10,7 +10,7 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
-    @Test
+    /*@Test
     public void addition_isCorrect(){
-    }
+    }*/
 }
