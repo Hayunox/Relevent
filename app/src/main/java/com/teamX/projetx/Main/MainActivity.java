5173f51ebcd2378bbee56992243a85e4c95aa99e
Paul Bouquet <paul.bouquet@gmail.com>
2017-07-09 21:22:38 +0200

    #25 : Implement Loading screen

--- app/src/main/java/com/teamX/projetx/Main/MainActivity.java
+++ app/src/main/java/com/teamX/projetx/Main/MainActivity.java
@@ -1,7 +1,9 @@
-package com.t.projetx;
+package com.teamX.projetx.Main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.t.projetx.R;
 
 public class MainActivity extends AppCompatActivity {
 
