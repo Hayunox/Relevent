5a5afc0cb2af015f7155d27486b353a44965e137
Paul Bouquet <paul.bouquet@gmail.com>
2017-07-17 19:49:29 +0200

    #2 repaire android

--- android/app/src/main/java/com/teamX/projetx/database/DataBase.java
+++ android/app/src/main/java/com/teamX/projetx/database/DataBase.java
@@ -10,7 +10,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
  */
 
 public class DataBase {
-    private static final String serverUrl = "http://192.168.2.82:8080/projetX/index.php/";
+    private static final String serverUrl = "http://192.168.2.82/projetX/index.php/";
 
     public static Retrofit getRetrofitService(){
         return new Retrofit.Builder()
