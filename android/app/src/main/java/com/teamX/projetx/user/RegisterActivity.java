b99b366b05453f4e8e85b427039741bce28be4f6
Bahrayn <djeanbastien@gmail.com>
2017-07-31 19:51:34 +0200

    #27

--- android/app/src/main/java/com/teamX/projetx/user/RegisterActivity.java
+++ android/app/src/main/java/com/teamX/projetx/user/RegisterActivity.java
@@ -3,6 +3,7 @@ package com.teamX.projetx.user;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
@@ -35,6 +36,10 @@ public class RegisterActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbarUserRegister);
+        setSupportActionBar(toolbar);
+        if(getSupportActionBar() != null)
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         // set up the interface
         this.register       = (Button) findViewById(R.id.buttonUserRegistrationRegister);
