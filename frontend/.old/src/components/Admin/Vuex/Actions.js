24e0994f6f5f927e56d27a64f7c1ecd2a5e6a754
Paul Bouquet <paul.bouquet@gmail.com>
2017-08-11 18:29:36 +0200

    #68

--- frontend/.old/src/components/Admin/Vuex/Actions.js
+++
@@ -1,6 +0,0 @@
-// An action will receive the store as the first argument.
-// Since we are only interested in the dispatch (and optionally the state)
-// we can pull those two parameters using the ES6 destructuring feature
-export const toggleSiteNavVisible = function ({dispatch, state}) {
-  dispatch('TOGGLE_SITENAV_VISIBLE')
-}
