811b79fb116f9848aacf2f041e84e8434b9c7d86
Paul Bouquet <paul.bouquet@gmail.com>
2017-08-12 16:00:33 +0200

    #68 configure semantic on react + change projet name to Relevent

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css';
 
 class App extends Component {
   render() {
